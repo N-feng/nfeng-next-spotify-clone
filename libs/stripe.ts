a3f3c05242516e9d5456e3866081a909156409ae
N-feng <308561157@qq.com>
2024-08-30 23:03:16 +0800

    fix🐛: fix stripe api version bug

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ?? '',
   {
-    apiVersion: '2024-06-20',
+    apiVersion: '2022-11-15',
     appInfo: {
       name: 'Spotify Clone Video',
       version: '0.1.0'
